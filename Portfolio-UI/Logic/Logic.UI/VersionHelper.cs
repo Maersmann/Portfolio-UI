d851c23ed9e524c0cede08cd32c759ef7f8bbe28
Rene Märsmann <renemaersmann@gmail.com>
2021-08-03 13:36:20 +0200

    Finish #97

---
+++ Portfolio-UI/Logic/Logic.UI/VersionHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Logic.Core
+{
+    public class VersionHelper
+    {
+        public string GetVersion => "2.3.0";
+        public DateTime GetRelease => new DateTime(2021, 7, 31);
+    }
+}
