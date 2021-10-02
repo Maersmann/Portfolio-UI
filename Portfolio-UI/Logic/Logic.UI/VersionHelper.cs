45e528ae5f62e5b1ef243ed9a54bee357bd6cf05
Rene Märsmann <renemaersmann@gmail.com>
2021-10-02 16:24:55 +0200

    Vorbereitung Release

--- Portfolio-UI/Logic/Logic.UI/VersionHelper.cs
+++ Portfolio-UI/Logic/Logic.UI/VersionHelper.cs
@@ -7,7 +7,7 @@ namespace Logic.Core
 {
     public class VersionHelper
     {
-        public string GetVersion => "2.4.0";
-        public DateTime GetRelease => new DateTime(2021, 8, 14);
+        public string GetVersion => "2.5.0";
+        public DateTime GetRelease => new DateTime(2021, 10, 2);
     }
 }
