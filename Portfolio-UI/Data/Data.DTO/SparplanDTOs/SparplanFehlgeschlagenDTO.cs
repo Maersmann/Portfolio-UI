e969d0923231c8d213f62770e7f31a87449413d6
Rene Märsmann <renemaersmann@gmail.com>
2022-10-18 18:57:34 +0200

    Finish #155

---
+++ Portfolio-UI/Data/Data.DTO/SparplanDTOs/SparplanFehlgeschlagenDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.DTO.SparplanDTOs
+{
+    public class SparplanFehlgeschlagenDTO
+    {
+        public int SparplanID { get; set; }
+    }
+}
---
+++ Portfolio-UI/Data/Data.Types/SparplanTypes/SparplanHistoryArt.cs
@@ -0,0 +1,17 @@
+﻿using Base.Logic.Types.Converter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Data.Types.SparplanTypes
+{
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
+    public enum SparplanHistoryArt
+    {
+        [Description("Ausgeführt")]
+        ausgefuehrt = 0,
+        [Description("Fehlgeschlagen")]
+        fehlgeschlagen = 1
+    }
+}
